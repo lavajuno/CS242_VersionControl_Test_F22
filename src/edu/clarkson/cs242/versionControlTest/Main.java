ba250de73882fb42e6e20a70eb73fc7183b3f805
TheEvilDemonicFlower <104311675+TheEvilDemonicFlower@users.noreply.github.com>
2022-10-04 17:54:58 -0400

    This is a comment

--- src/edu/clarkson/cs242/versionControlTest/Main.java
+++ src/edu/clarkson/cs242/versionControlTest/Main.java
@@ -3,5 +3,6 @@ package edu.clarkson.cs242.versionControlTest;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world from Person B");
     }
 }
